aea80e92c3d96b6bc020e027ad39e472f779f9fe
nishi <snishendra@student.ethz.ch>
2022-06-30 15:44:50 +0200

    extended mod init to head

--- segmentation_models_pytorch/base/initialization.py
+++ segmentation_models_pytorch/base/initialization.py
@@ -7,7 +7,8 @@ def initialize_decoder(module):
 
         if isinstance(m, nn.Conv2d):
             
-            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels  # perhaps in_channels? this might be backpropagation
+            # alternate initialization from noemi
+            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels  
             nn.init.normal_(m.weight, mean=0, std=math.sqrt(2. / ((1 + math.pow(INIT_A, 2)) * n)))
 
             # nn.init.kaiming_uniform_(m.weight, mode="fan_in", nonlinearity="relu")
@@ -26,7 +27,21 @@ def initialize_decoder(module):
 
 def initialize_head(module):
     for m in module.modules():
-        if isinstance(m, (nn.Linear, nn.Conv2d)):
+        # if isinstance(m, (nn.Linear, nn.Conv2d)):
+        #     nn.init.xavier_uniform_(m.weight)
+        #     if m.bias is not None:
+        #         nn.init.constant_(m.bias, 0)
+
+        if isinstance(m, nn.Linear):
             nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        
+        if isinstance(m, nn.Conv2d):
+            
+            # alternate initialization from noemi
+            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels  
+            nn.init.normal_(m.weight, mean=0, std=math.sqrt(2. / ((1 + math.pow(INIT_A, 2)) * n)))
+
             if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
